2019836b1391a63b7c4b898706ccb4c0212bee78
Douglas <douglasmodolo@outlook.com.br>
2025-04-26 19:39:14 -0300

    refactor: remove desnecessary method test

--- tests/JornadaMilhas.Test/OfertaViagemConstrutorTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutorTest.cs
@@ -41,22 +41,5 @@ namespace JornadaMilhas.Test
 							oferta.Erros.Sumario.Normalize(NormalizationForm.FormC));
 			Assert.False(oferta.EhValido);
 		}
-
-		[Fact]
-		public void OfertaViagem_PrecoNegativo_DeveRetornarErro()
-		{
-			// Arrange
-			Rota rota = new Rota("Origem", "Destino");
-			Periodo periodo = new Periodo(new DateTime(2025, 4, 1), new DateTime(2025, 4, 10));
-			double preco = -100.0;
-
-			// Act
-			OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
-
-			// Assert
-			Assert.Contains("O preço da oferta de viagem deve ser maior que zero.".Normalize(NormalizationForm.FormC),
-							oferta.Erros.Sumario.Normalize(NormalizationForm.FormC));
-			Assert.False(oferta.EhValido);
-		}
 	}
 }
