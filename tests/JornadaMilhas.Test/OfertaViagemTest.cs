3d3e8ce7a97d4ec949470587a475894d23ea2d44
Douglas <douglasmodolo@outlook.com.br>
2025-04-26 17:54:38 -0300

    test: add new tests for oferta viagem

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -36,5 +36,21 @@ namespace JornadaMilhas.Test
 			Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
 			Assert.False(oferta.EhValido);
 		}
+
+		[Fact]
+		public void OfertaViagem_PrecoNegativo_DeveRetornarErro()
+		{
+			// Arrange
+			Rota rota = new Rota("Origem", "Destino");
+			Periodo periodo = new Periodo(new DateTime(2025, 4, 1), new DateTime(2025, 4, 10));
+			double preco = -100.0;
+			
+			// Act
+			OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+			
+			// Assert
+			Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+			Assert.False(oferta.EhValido);
+		}
 	}
 }
