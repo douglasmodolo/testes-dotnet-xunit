2c0c5bc8fa3029eca7a6616cf7bd5f0a6326d7d7
Douglas <douglasmodolo@outlook.com.br>
2025-04-26 18:06:16 -0300

    tests: add normalizeString method

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -1,9 +1,21 @@
 using JornadaMilhasV1.Modelos;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 
 namespace JornadaMilhas.Test
 {
 	public class OfertaViagemTest
 	{
+		public static string NormalizeString(string input)
+		{
+			return input.Normalize(NormalizationForm.FormD)
+						.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+						.Aggregate(new StringBuilder(), (sb, c) => sb.Append(c))
+						.ToString()
+						.Normalize(NormalizationForm.FormC);
+		}
+
 		[Fact]
 		public void OfertaViagem_RotaPeriodoPrecoValidos_DeveSerValida()
 		{
@@ -33,7 +45,8 @@ namespace JornadaMilhas.Test
 			OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
 			// Assert
-			Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+			Assert.Contains(NormalizeString("A oferta de viagem não possui rota ou período válidos."),
+							NormalizeString(oferta.Erros.Sumario));
 			Assert.False(oferta.EhValido);
 		}
 
@@ -44,12 +57,13 @@ namespace JornadaMilhas.Test
 			Rota rota = new Rota("Origem", "Destino");
 			Periodo periodo = new Periodo(new DateTime(2025, 4, 1), new DateTime(2025, 4, 10));
 			double preco = -100.0;
-			
+
 			// Act
 			OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
-			
+
 			// Assert
-			Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+			Assert.Contains(NormalizeString("O preço da oferta de viagem deve ser maior que zero."),
+							NormalizeString(oferta.Erros.Sumario));
 			Assert.False(oferta.EhValido);
 		}
 	}
